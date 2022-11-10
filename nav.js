ac743b06b2f3e2dd2d9de96c6fefe667dfb79bf3
dashax <dariadenejkina@gmail.com>
2022-11-09 20:51:26 -0500

    Nav Changes

--- nav.js
+++ nav.js
@@ -1,4 +1,9 @@
-fetch('NavStudent.html')
+let currentPage = window.location.pathname
+
+console.log(currentPage);
+
+if(currentPage == "/MainStudent.html"){
+    fetch('NavStudent.html')
 .then(res => res.text())
 .then(text => {
     let oldelem = document.querySelector("script#replace_with_navbar");
@@ -6,3 +11,19 @@ fetch('NavStudent.html')
     newelem.innerHTML = text;
     oldelem.parentNode.replaceChild(newelem,oldelem);
 })
+}
+else if(currentPage == "/MainTeacher.html"){
+    fetch('NavTeacher.html')
+    .then(res => res.text())
+    .then(text => {
+        let oldelem = document.querySelector("script#replace_with_navbar");
+        let newelem = document.createElement("div");
+        newelem.innerHTML = text;
+        oldelem.parentNode.replaceChild(newelem,oldelem);
+    })
+}
+
+
+
+
+
