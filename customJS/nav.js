87bdb696b152d313ab982ad771166e829edf398d
Jose Sanchez <josefelixsanchezb@gmail.com>
2022-11-27 21:35:24 -0500

    fixed the issue of the nav bar not showing up

--- customJS/nav.js
+++ customJS/nav.js
@@ -2,26 +2,26 @@ let currentPage = window.location.pathname
 
 console.log(currentPage);
 
-if(currentPage == "/MainStudent.html" || currentPage == "/participantView.html" ||
-    currentPage == "/AsseStudent.html" || currentPage == "/StatsView.html"){
+if(currentPage.endsWith("MainStudent.html") || currentPage.endsWith("participantView.html") ||
+    currentPage.endsWith("AsseStudent.html") || currentPage.endsWith("StatsView.html")){
     fetch('NavStudent.html')
-.then(res => res.text())
-.then(text => {
-    let oldelem = document.querySelector("script#replace_with_navbar");
-    let newelem = document.createElement("div");
-    newelem.innerHTML = text;
-    oldelem.parentNode.replaceChild(newelem,oldelem);
-})
+        .then(res => res.text())
+        .then(text => {
+            let oldelem = document.querySelector("script#replace_with_navbar");
+            let newelem = document.createElement("div");
+            newelem.innerHTML = text;
+            oldelem.parentNode.replaceChild(newelem,oldelem);
+        })
 }
-else if(currentPage == "/MainTeacher.html" || currentPage == "/TeacherGrade.html"
-        || currentPage == "/TeacherGrade2.html" || currentPage == "/TeacherGrade3.html"
-        || currentPage == "/TeacherMidterm.html" || currentPage == "/TeacherFinal.html"){
+else if(currentPage.endsWith("MainTeacher.html") || currentPage.endsWith("TeacherGrade.html")
+    || currentPage.endsWith("TeacherGrade2.html")  || currentPage.endsWith("TeacherGrade3.html")
+    || currentPage.endsWith("TeacherMidterm.html") || currentPage.endsWith("TeacherFinal.html")){
     fetch('NavTeacher.html')
-    .then(res => res.text())
-    .then(text => {
-        let oldelem = document.querySelector("script#replace_with_navbar");
-        let newelem = document.createElement("div");
-        newelem.innerHTML = text;
-        oldelem.parentNode.replaceChild(newelem,oldelem);
-    })
+        .then(res => res.text())
+        .then(text => {
+            let oldelem = document.querySelector("script#replace_with_navbar");
+            let newelem = document.createElement("div");
+            newelem.innerHTML = text;
+            oldelem.parentNode.replaceChild(newelem,oldelem);
+        })
 }
